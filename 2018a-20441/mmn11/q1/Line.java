1f14389903d428a16d884698a8c94ff21c424761
Tal Glanzman <talglanzman@gmail.com>
2017-10-29 19:55:13 +0200

    20441 q1

---
+++ 2018a-20441/mmn11/q1/Line.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Line
+{
+    public static void main(String[] args)
+    {
+        // Acquire input from the user. two 2d points
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter 4 integers");
+        System.out.println("Please enter x1:");
+        int x1 = scan.nextInt();
+        System.out.println("Please enter y1:");
+        int y1 = scan.nextInt();
+        System.out.println("Please enter x2:");
+        int x2 = scan.nextInt();
+        System.out.println("Please enter x2:");
+        int y2 = scan.nextInt();
+        
+        // Calculate the distance using the formula: [(x1-x2)^2 + (y1-y2)^2]^0.5
+        double xDeltaSquared = Math.pow(x1 - x2, 2);
+        double yDeltaSquared = Math.pow(y1 - y2, 2);
+        double distance = Math.sqrt(xDeltaSquared + yDeltaSquared);
+        
+        // Output the result to the user
+        System.out.println(
+            "The length of the line between the points " +
+            "(" + x1 + "," + y1 + ") and " +
+            "(" + x2 + "," + y2 + ") is " +
+            distance
+        );
+    }
+}
