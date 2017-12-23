836eb957470f28125a6d7a7ae2bc3addea83fc4c
Tal Glanzman <talglanzman@gmail.com>
2017-12-23 16:07:35 +0200

    Fix

--- 2018a-20441/mmh02/Q12to18/Driver.java
+++ 2018a-20441/mmh02/Q12to18/Driver.java
@@ -10,22 +10,22 @@ public class Driver {
         System.out.println("12. expecting x, false: "); 
         System.out.println(a3.equals(a1));
 
-        System.out.println("13. expecting 2, true: ");
+        System.out.println("13. expecting 1, true: ");
         System.out.println(a4.equals(a2));
 
         System.out.println("14. expecting x, false: ");
         System.out.println(a1.equals(a2));
 
-        System.out.println("15. expecting 3, true: ");
+        System.out.println("15. expecting 1, true: ");
         System.out.println(a2.equals(b1));
 
         System.out.println("16. expecting 2, false: ");
         System.out.println(b1.equals(a1));
 
-        System.out.println("17. expecting : ");
+        System.out.println("17. expecting : 3, true");
         System.out.println(b2.equals(b1));
 
-        System.out.println("18. expecting : ");
+        System.out.println("18. expecting : 2, true");
         System.out.println(b1.equals(a4));
     }
 }
