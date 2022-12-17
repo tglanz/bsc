024d05fc8d7e3a8225ed9a910c1d040580b1efef
Tal Glanzman <talglanzman@gmail.com>
2022-12-17 12:27:57 +0200

    improve render script

--- planks/render.sh
+++ planks/render.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
-#
 
+argument=$1
 
-# pandoc main.md \
-#  -f markdown+footnotes \
-#  -V dir:"rtl" -V theme:"boxes" -V mainfont:"Times New Roman" -V lang:"he-IL" \
-#  -V navigation:"horizontal" \
-#  --pdf-engine=xelatex \
-#  -s -i --slide-level=2 -t beamer -o out.pdf
+function outputPDF() {
+  pandoc main.md \
+     -f markdown+footnotes \
+     -V dir:"rtl" -V theme:"boxes" -V mainfont:"Times New Roman" -V lang:"he-IL" \
+     -V navigation:"horizontal" \
+     --pdf-engine=xelatex \
+     -s -i --slide-level=2 -t beamer -o out.pdf
+}
 
-pandoc main.md \
-  -V dir:"rtl" -V mainfont:"Times New Roman" -V lang:"he-IL" \
-  --pdf-engine=xelatex \
-  -s -i --slide-level=2 -t revealjs -o out.html
+function outputHTML() {
+  pandoc main.md \
+    -V dir:"rtl" -V mainfont:"Times New Roman" -V lang:"he-IL" \
+    --pdf-engine=xelatex \
+    -s -i --slide-level=2 -t revealjs -o out.html
+}
+
+case $argument in
+  all)
+    outputPDF
+    outputHTML
+    ;;
+  pdf)
+    outputPDF
+    ;;
+  html)
+    outputHTML
+    ;;
+  *) ;;
+esac
