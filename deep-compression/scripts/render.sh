af5443f0fedeb12fb698ab95d3da7fd69c254f71
Tal Glanzman <talglanzman@gmail.com>
2023-05-26 16:38:07 +0300

    link style

--- deep-compression/scripts/render.sh
+++ deep-compression/scripts/render.sh
@@ -18,18 +18,8 @@ function renderAll() {
      --toc -s -i -o output/all.pdf
 }
 
-function renderPruning() {
-  mkdir -p output
-  pandoc chapters/*pruning.md chapters/*references.md \
-     -f markdown+footnotes+implicit_figures \
-     -V mainfont:"Times New Roman" \
-     --pdf-engine=xelatex \
-     --toc -s -i -o output/pruning.pdf
-}
-
 case $target in
   all) renderAll;;
-  pruning) renderPruning;;
   *) error "unknown target: $target"
 esac
 # outputPDF
