5feb4caf52d9c1d062ad9e5f8e819b1b1aef9ee3
Tal Glanzman <talglanzman@gmail.com>
2024-04-19 13:38:25 +0300

    fixes and tests for mmn12

--- 2024b-22961/mmn12/q2.py
+++ 2024b-22961/mmn12/q2.py
@@ -1,6 +1,9 @@
-from typing import List, Mapping, Optional 
-from pprint import pp
+from __future__ import annotations
+
+from typing import Callable, List, Mapping, Optional, Type 
 import math
+from random import random
+import torch
 
 class MyScalar:
 
@@ -17,205 +20,228 @@ class MyScalar:
         self.parents = [] if parents is None else parents
         self.grad = { id(self): 1.0 } if grad is None else grad
 
-def add(lhs: MyScalar, rhs: MyScalar) -> MyScalar:
-    value = lhs.value + rhs.value
-    parents = [lhs, rhs]
+    def add(self: MyScalar, other: float) -> MyScalar:
+        value = self.value + other 
+        parents = [self]
 
-    grad = {
-            id(lhs): 1,
-            id(rhs): 1,
-            }
+        grad = {
+                id(self): 1,
+                }
 
-    return MyScalar(value, parents, grad)
+        return MyScalar(value, parents, grad)
 
-def sub(lhs: MyScalar, rhs: MyScalar) -> MyScalar:
-    value = lhs.value - rhs.value
-    parents = [lhs, rhs]
-    grad = {
-            id(lhs): 1,
-            id(rhs): -1,
-            }
+    def __add__(self: MyScalar, rhs: MyScalar) -> MyScalar:
+        value = self.value + rhs.value
+        parents = [self, rhs]
 
-    return MyScalar(value, parents, grad)
+        grad = {
+                id(self): 1,
+                id(rhs): 1,
+                }
 
-def mul(lhs: MyScalar, rhs: MyScalar) -> MyScalar:
-    value = lhs.value * rhs.value
-    parents = [lhs, rhs]
-    grad = {
-            id(lhs): rhs.value,
-            id(rhs): lhs.value
-            }
-    return MyScalar(value, parents, grad)
+        return MyScalar(value, parents, grad)
 
-def div(dividend: MyScalar, divisor: MyScalar) -> MyScalar:
-    assert divisor != 0, "Divisor should be non-zero"
-    value = dividend.value * divisor.value
-    parents = [dividend, divisor]
-    grad = {
-            id(dividend): 1 / divisor.value,
-            id(divisor): dividend.value
-            }
-    return MyScalar(value, parents, grad)
+    def __sub__(self: MyScalar, rhs: MyScalar) -> MyScalar:
+        value = self.value - rhs.value
+        parents = [self, rhs]
+        grad = {
+                id(self): 1,
+                id(rhs): -1,
+                }
 
-def power(base: MyScalar, exponent: float) -> MyScalar:
-    value = math.pow(base.value, exponent)
-    parents = [base]
-    grad = { id(base): exponent * math.pow(value, exponent - 1) }
-    return MyScalar(value, parents, grad)
+        return MyScalar(value, parents, grad)
 
-def exp(exponent: MyScalar) -> MyScalar:
-    value = math.exp(exponent.value)
-    parents = [exponent]
-    grad = { id(exponent): value }
-    return MyScalar(value, parents, grad)
+    def sub(self: MyScalar, rhs: float) -> MyScalar:
+        value = self.value - rhs
+        parents = [self]
 
-def cos(theta: MyScalar) -> MyScalar:
-    value = math.cos(theta.value)
-    parents = [theta]
-    grad = { id(theta): -1 * math.sin(theta.value) }
-    return MyScalar(value, parents, grad)
+        grad = {
+                id(self): 1,
+                }
 
-def sin(theta: MyScalar) -> MyScalar:
-    value = math.sin(theta.value)
-    parents = [theta]
-    grad = { id(theta): math.cos(theta.value) }
-    return MyScalar(value, parents, grad)
+        return MyScalar(value, parents, grad)
 
-def ln(operand: MyScalar) -> MyScalar:
-    assert operand.value > 0, "Logarithm operand must be positive"
+    def __mul__(self: MyScalar, rhs: MyScalar) -> MyScalar:
+        value = self.value * rhs.value
+        parents = [self, rhs]
+        grad = {
+                id(self): rhs.value,
+                id(rhs): self.value
+                }
+        return MyScalar(value, parents, grad)
 
-    value = math.log(operand.value)
-    parents = [operand]
-    grad = { id(operand): 1 / operand.value }
-    return MyScalar(value, parents, grad)
+    def mul(self: MyScalar, rhs: float) -> MyScalar:
+        value = self.value * rhs
+        parents = [self]
+        grad = {
+                id(self): rhs,
+                }
+        return MyScalar(value, parents, grad)
 
+    def __truediv__(self: MyScalar, divisor: MyScalar) -> MyScalar:
+        assert divisor.value != 0, "Divisor should be non-zero"
+        value = self.value / divisor.value
+        parents = [self, divisor]
+        grad = {
+                id(self): 1 / divisor.value,
+                id(divisor): self.value * -1 / (divisor.value**2)
+                }
+        return MyScalar(value, parents, grad)
 
-def get_gradient(
-        operand: MyScalar,
-        aliases: Optional[Mapping[int, str]] = None,
-) -> Mapping[str, float]:
+    def div(self: MyScalar, divisor: float) -> MyScalar:
+        assert divisor != 0, "Divisor should be non-zero"
+        value = self.value * divisor
+        parents = [self, divisor]
+        grad = {
+                id(self): 1 / divisor,
+                }
+        return MyScalar(value, parents, grad)
 
-    # immediate gradients are the basecase
-    grad = dict(operand.grad)
+    def pow(self: MyScalar, exponent: float) -> MyScalar:
+        value = math.pow(self.value, exponent)
+        parents = [self]
+        grad = { id(self): exponent * math.pow(self.value, exponent - 1) }
+        return MyScalar(value, parents, grad)
 
-    # run dfs on the graph, compute the derivatives according to the chain rule.
-    stack = list(operand.parents)
+    def exp(self: MyScalar) -> MyScalar:
+        value = math.exp(self.value)
+        parents = [self]
+        grad = { id(self): value }
+        return MyScalar(value, parents, grad)
 
-    while stack:
-        node = stack.pop()
+    def cos(self: MyScalar) -> MyScalar:
+        value = math.cos(self.value)
+        parents = [self]
+        grad = { id(self): -1 * math.sin(self.value) }
+        return MyScalar(value, parents, grad)
 
-        for parent in node.parents:
-            # addititive residuals, multiplicative chain rule 
-            grad[id(parent)] = grad.get(id(parent), 0) + (grad[id(node)] * node.grad[id(parent)])
-            stack.append(parent)
+    def sin(self: MyScalar) -> MyScalar:
+        value = math.sin(self.value)
+        parents = [self]
+        grad = { id(self): math.cos(self.value) }
+        return MyScalar(value, parents, grad)
 
-    ans = {}
-    aliases = aliases or {}
-    for k, v in (grad).items():
-        if k in aliases:
-            ans[aliases.get(k, str(k))] = v
+    def log(self: MyScalar) -> MyScalar:
+        assert self.value > 0, "Logarithm operand must be positive"
 
-    return ans
-
-def f():
-    a = MyScalar(2)
-    b = power(a, 2)
-    c = exp(b)
-
-    aliases = {
-        id(a): "a",
-        id(b): "b",
-        id(c): "c"
-        }
+        value = math.log(self.value)
+        parents = [self]
+        grad = { id(self): 1 / self.value }
+        return MyScalar(value, parents, grad)
 
 
-    grad_c = get_gradient(c, aliases)
+    def get_gradient(
+            self: MyScalar,
+            aliases: Optional[Mapping[int, str]] = None,
+    ) -> Mapping[str, float]:
 
-    pp(aliases, indent=4)
-    pp(grad_c, indent=4)
+        # immediate gradients are the basecase
+        grad = dict(self.grad)
 
-def g():
-    a = MyScalar(1)
-    b = MyScalar(2)
-    c = add(a, b)
+        # run dfs on the graph, compute the derivatives according to the chain rule.
+        stack = list(self.parents)
 
+        while stack:
+            node = stack.pop()
 
-    aliases = {
-        id(a): "a",
-        id(b): "b",
-        id(c): "c"
-        }
+            for parent in node.parents:
+                # addititive residuals, multiplicative chain rule 
+                grad[id(parent)] = grad.get(id(parent), 0) + (grad[id(node)] * node.grad[id(parent)])
+                stack.append(parent)
 
+        ans = {}
+        aliases = aliases or {}
+        for k, v in (grad).items():
+            if k in aliases:
+                ans[aliases.get(k, str(k))] = v
 
-    grad_c = get_gradient(c, aliases)
+        return ans
 
-    pp(aliases, indent=4)
-    pp(grad_c, indent=4)
+class Tester:
+    @staticmethod
+    def compute_torch_grad(initializers, f):
+        tensors = [
+                torch.tensor(initializer, requires_grad=True)
+                for initializer
+                in initializers]
 
-def test_mul():
-    a = MyScalar(1)
-    b = MyScalar(2)
-    c = mul(a, b)
+        ans = f(*tensors)
+        ans.backward()
 
-    aliases = {
-        id(a): "a",
-        id(b): "b",
-        id(c): "c"
-        }
+        grad = {}
+        for i, tensor in enumerate(tensors):
+            name = chr(ord('a') + i)
+            grad[name] = tensor.grad.item()
 
-    grad_c = get_gradient(c, aliases)
+        return grad
 
-    pp(aliases, indent=4)
-    pp(grad_c, indent=4)
+    @staticmethod
+    def compute_custom_grad(initializers, f):
+        myscalars = [
+                MyScalar(initializer)
+                for initializer
+                in initializers]
 
-def test_addmul():
-    a = MyScalar(2)
-    b = MyScalar(3)
-    c = MyScalar(4)
+        ans = f(*myscalars)
 
-    d = mul(b, c)
-    e = add(a, d)
+        aliases = {}
+        for i, myscalar in enumerate(myscalars):
+            name = chr(ord('a') + i)
+            aliases[id(myscalar)] = name
+        grad = ans.get_gradient(aliases)
+        return grad
 
-    aliases = {
-        id(a): "a",
-        id(b): "b",
-        id(c): "c",
-        id(d): "d",
-        id(e): "e"
-        }
+    @staticmethod
+    def error(a, b):
+        if max(a, b) == 0:
+            return min(a, b)
 
-    grad = get_gradient(e, aliases)
+        return abs(a - b) / max(a, b)
 
-    pp(aliases, indent=4)
-    pp(grad, indent=4)
+    @staticmethod
+    def run(name, argc, f):
+        print(f"{name}")
 
-def test_simple_residual():
-    a = MyScalar(2)
-    b = MyScalar(3)
-    c = MyScalar(4)
+        initializers = [random() for _ in range(argc)]
+        custom_grad = Tester.compute_custom_grad(initializers, f)
+        torch_grad = Tester.compute_torch_grad(initializers, f)
 
-    d = mul(a, b)
-    e = mul(a, c)
+        assert custom_grad.keys() == torch_grad.keys(), "Torch and Custom grads yielded different leafs"
 
-    f = add(d, e)
+        for k, custom_val in custom_grad.items():
+            torch_val = torch_grad[k]
+            error = Tester.error(custom_val, torch_val) 
+            print(f" - {k}, error={round(error, 3)}, custom_grad={custom_val}, torch_grad={torch_val}")
+            assert error < 0.1, f"Torch and Custom grad[{k}] differ"
 
-    aliases = {
-        id(a): "a",
-        id(b): "b",
-        id(c): "c",
-        id(d): "d",
-        id(e): "e",
-        id(f): "f",
-        }
-
-    grad = get_gradient(f, aliases)
-    print(grad)
 
 def main():
-    # test_addmul()
-    test_simple_residual()
-    # c_a = 218.392
-    # c_b = 54.598
+    test_cases = [
+            ("exp", 1, lambda a: a.exp()),
+
+            ("a + b", 2, lambda a, b: a + b),
+            ("a - b", 2, lambda a, b: a - b),
+            ("a * b", 2, lambda a, b: a * b),
+            ("a / b", 2, lambda a, b: a / b),
+
+            ("ln", 1, lambda a: a.log()),
+
+            ("large power", 1, lambda a: a.pow(5)),
+            ("zero power", 1, lambda a: a.pow(0)),
+            ("fractional power", 1, lambda a: a.pow(0.3)),
+            ("negative power", 1, lambda a: a.pow(-4.0)),
+
+            ("cos", 1, lambda a: a.cos()),
+            ("sin", 1, lambda a: a.sin()),
+
+            ("complex graph, graph such that a is used in more than one branch",
+             4,
+             lambda a, b, c, d: ((a.log() / b.sin()) + (a.cos() * c) - (a + d.exp())).pow(2)
+             )
+        ]
+
+    for name, argc, f in test_cases:
+        Tester.run(name, argc, f)
 
 if __name__ == '__main__':
     main()
