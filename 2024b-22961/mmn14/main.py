6dde9d3a2d47633fff14d30284aa7241db3aac7a
Tal Glanzman <talglanzman@gmail.com>
2024-05-24 18:03:39 +0300

    SplitLayer

--- 2024b-22961/mmn14/main.py
+++ 2024b-22961/mmn14/main.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import numpy as np
 from torch.utils.data import DataLoader
+import math
 
 class SplitLayer(nn.Module):
 
@@ -11,7 +12,7 @@ class SplitLayer(nn.Module):
     split_dim: int
 
     def __init__(self, input_batches: int, input_features: int):
-        super(SplitLayer, self).__init__()
+        super().__init__()
 
         assert input_batches > 0, "expected input_batches to be positive"
         assert input_features % 2 == 0, "expected input_features to be even"
@@ -26,11 +27,22 @@ class SplitLayer(nn.Module):
             nn.Linear(self.split_size, self.split_size),
             nn.ReLU())
 
+        self.initialize_weights()
+
+    def initialize_weights(self):
+        """ Weight Initialization using Xavier's method """
+        # sqrt(6 / (split_size + split_size))
+        xavier = math.sqrt(3 / self.split_size) 
+
+        with torch.no_grad():
+            self.hidden[0].weight.uniform_(-xavier, +xavier)
+
+        # Same as: (in fact I saw how to manually do it there)
+        # torch.nn.init.uniform_(self.hidden[0].weight, -xavier, +xavier)
+
     def forward(self, x: torch.Tensor):
         a_in, b_in = x.split(split_size=self.split_size, dim=self.split_dim)
 
-        print("x shape", x.shape)
-        print("a shape", a_in.shape)
         a_out = self.hidden(a_in)
         b_out = self.hidden(b_in)
 
