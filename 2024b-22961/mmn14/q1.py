97fceded17817c31533f2eb29185245bf5947192
Tal Glanzman <talglanzman@gmail.com>
2024-05-29 17:38:59 +0300

    sync q1

--- 2024b-22961/mmn14/q1.py
+++ 2024b-22961/mmn14/q1.py
@@ -3,11 +3,28 @@ import torch.nn as nn
 import numpy as np
 from torch.utils.data import DataLoader
 import math
-
 class SplitLayer(nn.Module):
+    """ 
+    An implementation of a neural network layer that given an input X it does the following:
+    - Split X into two halves X1, X2
+    - Feed X1, X2 into the same LinearLayer yielding outputs Z1, Z2
+    - Feed Z1, Z2 into a ReLU layer yielding outputs Y1, Y2
+    - Concatenate Y=[Y1, Y2]
+    """
+
+    # The number of input batches
     input_batches: int
+
+    # The number of input features.
+    # Because we split the input to two halves, this is expected to be even.
     input_features: int
+
+    # Determines the size of each split.
+    # It is basically input_features/2
     split_size: int
+
+    # Determines the dimension to split by.
+    # It is basically 1 since 1 is the expected features dimension.
     split_dim: int
 
     def __init__(self, input_batches: int, input_features: int):
@@ -29,7 +46,14 @@ class SplitLayer(nn.Module):
         self.initialize_weights()
 
     def initialize_weights(self):
-        """ Weight Initialization using Xavier's method """
+        """
+        Weight Initialization using Xavier's method.
+
+        As explained here
+        - https://d2l.ai/chapter_multilayer-perceptrons/numerical-stability-and-init.html#xavier-initialization
+        
+        Xavier's method tackles the issue of exploding/vanishing gradients 
+        """
         # sqrt(6 / (split_size + split_size))
         xavier = math.sqrt(3 / self.split_size) 
 
@@ -39,40 +63,36 @@ class SplitLayer(nn.Module):
         # Same as: (in fact I saw how to manually do it there)
         # torch.nn.init.uniform_(self.hidden[0].weight, -xavier, +xavier)
 
-    def forward(self, x: torch.Tensor, verbose=False):
+        # Bias is kept as is (0 bias)
 
+    def forward(self, x: torch.Tensor, verbose=False):
         log = print if verbose else lambda x: ()
 
-        log(f"Input: {x}")
+        log(f"  Input: {x}")
 
         a_in, b_in = x.split(split_size=self.split_size, dim=self.split_dim)
-        log(f"Split A: {a_in}")
-        log(f"Split B: {b_in}")
-
+        log(f"  Split A: {a_in}")
+        log(f"  Split B: {b_in}")
 
         a_out = self.hidden(a_in)
-        log(f"Ouput A: {a_in}")
+        log(f"  Ouput A: {a_in}")
 
         b_out = self.hidden(b_in)
-        log(f"Output B: {b_in}")
+        log(f"  Output B: {b_in}")
 
         y = torch.concat((a_out, b_out), dim=self.split_dim)
-        log(f"Output: {y}")
+        log(f"  Output: {y}")
 
         return y
+
+def demonstrate_split_layer():
+    print("Demonstration of SplitLayer: ")
     
-    @staticmethod
-    def demonstrate():
-        x = torch.arange(12).reshape((3, 4)).float()
+    x = torch.arange(12).reshape((3, 4)).float()
+    
+    split_layer = SplitLayer(3, 4)
+    split_layer.train(False)
+    
+    split_layer.forward(x, verbose=True)
 
-        split_layer = SplitLayer(3, 4)
-        split_layer.train(False)
-        
-        split_layer.forward(x, verbose=True)
-
-
-def main():
-    SplitLayer.demonstrate()
-
-if __name__ == '__main__':
-    main()
+demonstrate_split_layer()
